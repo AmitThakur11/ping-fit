c4c007d76f56105f13236112e279325d87dd8068
Amit_thakur11 <amithakur081@gmail.com>
2022-05-12 11:20:54 +0530

    Refactor : logo component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LogoImg from "../../assets/logo.png";
+function Logo() {
+  return (
+    <div className="logo">
+          <img className="logoImg" src={LogoImg} alt="logo" />
+          <div className="logoName">
+            <p className="logoName_first">NOW</p>
+            <p className="logoName_last ">FIT</p>
+          </div>
+        </div>
+  )
+}
+
+export default Logo
--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,29 +1,23 @@
 import React, { useState ,useEffect} from "react";
 import axios from "axios"
 import "./style.css";
-import Logo from "../../assets/logo.png";
+
 import ProfileImg from "../../assets/dp.png";
+import Logo  from "../../components/Logo"
 import { HiOutlineSearch } from "react-icons/hi";
 function Header() {
   const [searchToggle, setSearchToggle] = useState(false);
   const toggleSearchAction = ()=>setSearchToggle((searchToggle)=>!searchToggle)
   useEffect(()=>{
     (async()=>{
-      const response = await axios.get("https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos")
-      console.log(response)
+      
     })()
     
   })
   return (
     <header className="header">
       <section className="headerOne">
-        <div className="logo">
-          <img className="logoImg" src={Logo} alt="logo" />
-          <div className="logoName">
-            <p className="logoName_first">NOW</p>
-            <p className="logoName_last ">FIT</p>
-          </div>
-        </div>
+       <Logo/>
       </section>
       <section className="headerTwo">
         <input className={!searchToggle ? "search searchHide" : "search"} placeholder="Search..." />
