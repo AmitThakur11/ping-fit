f262cb7590aa997705b70d05ae19799a4b135d6e
Amit_thakur11 <amithakur081@gmail.com>
2022-05-12 12:16:17 +0530

    Style : empty slide

--- src/components/EmptySlide/index.js
+++ src/components/EmptySlide/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import "./style.css"
 import EmptyImg from "../../assets/empty.svg";
 
 function EmptySlide() {
   return (
     <div className="emptySlide">
-      <section className="emptySlide__img">
-        <img src={EmptyImg} alt="empty" />
+    <section className ="emptySlide_text">
+    <p>Find your perfect workout <span className ="highlight">NOW</span>...</p>
+  </section>
+    
+    <section className="emptySlide_img">
+      <img src={EmptyImg} alt="empty" />
       </section>
-      <section>
-        <p>Find your perfect workout...</p>
-      </section>
+
     </div>
   );
 }
