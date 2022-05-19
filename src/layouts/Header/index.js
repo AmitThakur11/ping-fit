b609e1d98835bdbc86c8c12e976364943b2167b9
Amit_thakur11 <amithakur081@gmail.com>
2022-05-20 01:31:00 +0530

    Intendation

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -4,11 +4,19 @@ import ProfileImg from "../../assets/dp.png";
 import { Logo } from "../../components";
 import { HiOutlineSearch, HiX } from "react-icons/hi";
 import { useData } from "../../context/data";
-import {selectTag} from "../../utils/filters"
+import { selectTag } from "../../utils/filters";
 function Header() {
   const [searchToggle, setSearchToggle] = useState(false);
-  const { takeSearchInput, searchInput, clearSearch,getTags,videoData , setSelectedTags,selectedTags} = useData();
-  
+  const {
+    takeSearchInput,
+    searchInput,
+    clearSearch,
+    getTags,
+    videoData,
+    setSelectedTags,
+    selectedTags,
+  } = useData();
+
   const toggleSearchAction = () =>
     setSearchToggle((searchToggle) => !searchToggle);
   const closeSearchBar = () => {
@@ -18,10 +26,8 @@ function Header() {
 
   return (
     <header className="header">
-    
       <section className="headerOne">
         <Logo />
-       
       </section>
       <section className="headerTwo">
         <input
@@ -41,12 +47,19 @@ function Header() {
         <img src={ProfileImg} alt="profile" />
       </section>
       <section className="videoFilters">
-  {
-    getTags(videoData).map((data)=>{
-      return <div className ="tagItem"><input onChange ={()=>selectTag(data,selectedTags,setSelectedTags)} type='checkbox' checked = {selectedTags.includes(data)} /><span>{data}</span></div>
-    })
-  }
-  </section>
+        {getTags(videoData).map((data) => {
+          return (
+            <div className="tagItem">
+              <input
+                onChange={() => selectTag(data, selectedTags, setSelectedTags)}
+                type="checkbox"
+                checked={selectedTags.includes(data)}
+              />
+              <span>{data}</span>
+            </div>
+          );
+        })}
+      </section>
     </header>
   );
 }
